6255358067cbd1a5cc7f1dfce4616e7bdb50f6a9
Vitaly <vitgololobov2006@gmail.com>
2025-01-21 10:05:04 +0300

    add

--- app/src/main/java/com/example/vibecut/EditerActivity.java
+++ app/src/main/java/com/example/vibecut/EditerActivity.java
@@ -5,9 +5,13 @@ import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 
 import android.content.ClipData;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.MediaMetadataRetriever;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -19,6 +23,12 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import org.apache.commons.io.FilenameUtils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.LocalTime;
 import java.util.List;
 
 public class EditerActivity extends AppCompatActivity {
@@ -115,7 +125,156 @@ public class EditerActivity extends AppCompatActivity {
     }
 
     public void addProjectFiles(View view){
+        pickMedia();
 
+    }
+    private void pickMedia() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/* video/*"); // Выбор изображений и видео
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        startActivityForResult(intent, PICK_MEDIA_REQUEST);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_MEDIA_REQUEST && resultCode == RESULT_OK && data != null) {
+            if (data.getClipData() != null) {
+                // Если выбрано несколько файлов
+                int count = data.getClipData().getItemCount();
+                for (int i = 0; i < count; i++) {
+                    ClipData.Item item = data.getClipData().getItemAt(i);
+                    processingFile(item);
+                }
+            } else if (data.getData() != null) {
+                // Если выбран только один файл
+                processingFile(data.getData());
+            }
+        }
+    }
+    // Обработка каждого файла
+    private void processingFile(ClipData.Item item) {
+        Uri selectedMediaUri = item.getUri(); // Получаем Uri из ClipData.Item
+        processUri(selectedMediaUri);
+    }
+
+    private void processingFile(Uri selectedMediaUri) {
+        processUri(selectedMediaUri);
+    }
+
+    private void processUri(Uri selectedMediaUri) {
+        // Получаем имя файла из Uri
+        String fileName = getFileName(selectedMediaUri);
+        String mimeType = getContentResolver().getType(selectedMediaUri);
+        Uri preview = getPreview(mimeType, selectedMediaUri);
+        String typeMedia = "";
+        LocalTime duration = LocalTime.of(0,0);
+        if (mimeType.startsWith("image/")) {
+            typeMedia = "img";
+            duration = LocalTime.of(0, 3);
+        } else if (mimeType.startsWith("video/")){
+            typeMedia = "video";
+            try {
+                duration = getVideoDuration(selectedMediaUri);
+            } catch (IOException e) {
+                e.printStackTrace(); // Логируем ошибку
+                Toast.makeText(this, "Не удалось получить длительность видео.", Toast.LENGTH_SHORT).show();
+            }
+        }
+        MediaFile mediaFile = new MediaFile(fileName, preview, selectedMediaUri, duration, typeMedia);
+        // Добавляем MediaFile в проект
+        projectInfo.addMediaFile(mediaFile);
+        // Уведомляем адаптер об изменении данных
+        adapter.notifyItemInserted(MediaFiles.size() - 1);
+        JSONHelper.exportToJSON(this, projectInfo);
+    }
+
+
+    // Получение предпросмотра
+    private Uri getPreview(String mimeType, Uri selectedMediaUri) {
+        Uri preview = null;
+        if (mimeType != null) {
+            if (mimeType.startsWith("image/")) {
+                preview = selectedMediaUri;
+            } else if (mimeType.startsWith("video/")) {
+                try {
+                    preview = getVideoThumbnail(selectedMediaUri); // Метод для получения миниатюры видео
+                } catch (IOException e) {
+                    e.printStackTrace(); // Логируем ошибку
+                    Toast.makeText(this, "Не удалось получить миниатюру видео.", Toast.LENGTH_SHORT).show();
+                    return preview;
+                }
+            } else {
+                Toast.makeText(this, "Выбранный файл не является изображением или видео.", Toast.LENGTH_SHORT).show();
+                return preview;
+            }
+        }
+        return preview;
+    }
+
+    // Метод для получения имени файла из Uri
+    private String getFileName(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    int nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                    if (nameIndex != -1) { // Проверяем, что индекс не -1
+                        result = cursor.getString(nameIndex);
+                    }
+                }
+            } finally {
+                if (cursor != null) {
+                    cursor.close();
+                }
+            }
+        }
+        if (result == null) {
+            // Если имя не найдено, пробуем извлечь его из пути
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
+    }
+
+    //метод для сохранения миниатюры видео в папку проекта
+    private Uri savePreviewToFolderProject(String prName, Bitmap img, String nameFile){
+        File folder = new File(this.getFilesDir(), "VibeCutProjects/" + prName);
+        if (!folder.exists()) {
+            folder.mkdirs();
+        }
+
+        // Создаем файл для сохранения миниатюры
+        File file = new File(folder, nameFile + ".png"); // Вы можете изменить имя файла по своему усмотрению
+
+        try (FileOutputStream out = new FileOutputStream(file)) {
+            img.compress(Bitmap.CompressFormat.PNG, 100, out); // Сохраняем изображение в формате PNG
+        } catch (IOException e) {
+            e.printStackTrace(); // Обработка ошибок
+        }
+        return Uri.fromFile(file);
+    }
+
+    //Метод для получения миниатюры видео
+    private Uri getVideoThumbnail(Uri uri) throws IOException{
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        retriever.setDataSource(this, uri);
+        Bitmap bitmap = retriever.getFrameAtTime(0); // Получаем первый кадр
+        retriever.release();
+        return savePreviewToFolderProject(projectInfo.getName(), bitmap, FilenameUtils.removeExtension(getFileName(uri)));
+    }
+    private LocalTime getVideoDuration(Uri videoUri) throws IOException {
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        retriever.setDataSource(this, videoUri);
+        String time = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+        long durationInMillis = Long.parseLong(time);
+        retriever.release();
+
+        // Преобразуем длительность в LocalTime
+        return LocalTime.ofNanoOfDay(durationInMillis * 1_000_000);
     }
 }
 
